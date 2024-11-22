6e950838c4cc36e0799477523ca7fe6306638f39
king <102906132+Ggy-king@users.noreply.github.com>
2024-11-22 10:10:17 +0800

    Delete vue/vue-one directory

--- vue/vue-one/vue.config.js
+++
@@ -1,5 +0,0 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  // publicPath: './',   默认是publicPath: '/'  改成./的目的就是为了改成相对路径就可以直接运行了
-  transpileDependencies: true
-})
