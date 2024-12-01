7def058f4a26c8bb7ffc79eaa408d6d76bc4758c
king <102906132+Ggy-king@users.noreply.github.com>
2024-12-01 18:23:55 +0800

    Add files via upload

---
+++ nodeJS/基础.js
@@ -0,0 +1,15 @@
+// 1 nodeAPI
+/**
+ * 注意 浏览器API主要有DOM BOM ajax storage console 定时器 alert/confirm
+ * 浏览器的api肯定是不能运行在node环境中的 浏览器与node相似点是都是遵循ecmaScript语法
+ * nodeAPI有
+ * fs
+ * url
+ * http
+ * util
+ * console
+ * 定时器
+ * path
+ */
+
+// 2 global顶级对象 globalThis是指向顶级对象的 globalThis === global
