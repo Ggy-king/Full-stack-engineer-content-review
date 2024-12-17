9be937c6617d568c758ad3da99a81ac6f512bf7b
king <102906132+Ggy-king@users.noreply.github.com>
2024-12-17 19:04:14 +0800

    Add files via upload

--- nodeJS/会话控制.js
+++ nodeJS/会话控制.js
@@ -21,12 +21,12 @@
 const express = require('express')
 
 const app = express()
-app.get('/set',(req,res) => {
-    res.cookie('name','zhangsan')  // 这种设置cookie的方式会在浏览器关闭的时候自动销毁
-    res.cookie('name','lisi',{maxAge: 60 * 1000})  // 可以设置存储时间 即使浏览器销毁也会在 单位毫秒
+app.get('/set', (req, res) => {
+    res.cookie('name', 'zhangsan')  // 这种设置cookie的方式会在浏览器关闭的时候自动销毁
+    res.cookie('name', 'lisi', { maxAge: 60 * 1000 })  // 可以设置存储时间 即使浏览器销毁也会在 单位毫秒
 })
 
-app.get('/remove',(req,res) => {
+app.get('/remove', (req, res) => {
     res.clearCookie('name')  // 删除cookie
 
 })
@@ -34,7 +34,7 @@ app.get('/remove',(req,res) => {
 // 如何拿到请求的cookie 需要用到第三方包 cookie-parser
 const cookieParser = require('cookie-parser')
 app.use(cookieParser())  // 中间件
-app.get('/get',(req,res) => {
+app.get('/get', (req, res) => {
     req.cookies  // 获取cookie
 
 })
@@ -78,7 +78,7 @@ app.use(session({
 app.get('/login', (req, res) => {
     // 拿到query中username与password 
     if (req.query.username === 'admin' && req.query.password === '1234567') {
-        
+
         // 设置session 上面密码验证是其他逻辑 session只是满足存储用户信息功能 
         req.session.username = req.query.username
         req.session.password = req.query.password
@@ -93,7 +93,7 @@ app.get('/login', (req, res) => {
 app.get('/cart', (req, res) => {
     // 判断用户唯一标识是否存在 虽然你设置很多参数在标识里 但你只需要判断一个参数在不在即可插件会帮你分析的
     if (req.session.username) {
-        
+
         res.send(`你好，${req.session.username},您的密码是${req.session.password}`)
     } else {
         res.send('您未登录')
@@ -106,6 +106,47 @@ app.get('/logout', (req, res) => {
     req.session.destroy() // 销毁
     res.send('退出成功')
 })
+
+
+
+/**
+ * 3 token
+ * 很类似session 都是通过发送请求来确定用户身份以及信息
+ * 1 token是保存在用户端 需要手动发送 session是自动发送
+ * 2 数据更安全是全部加密 可以避免CSRF(因为token是在请求头里的 不通过cookie发送)
+ * 3 服务间可以共享 毕竟是在用户端 自然是可以携带着他访问不同服务
+ * 4 增加服务器的扩展性 多台服务器想要共享sessions只能复制数据库 但token在用户端保存无需扩展数据库
+ * 
+ * JWT目前最流行的身份验证 可用于基于token的身份验证
+ */
+
+// 1 安装 jsonwebtoken包 nodejs中引入
+const jwt = require('jsonwebtoken')
+
+// 2 创建token
+// jwt.sign(用户数据,加密字符串 ,配置对象)
+
+// const token = jwt.sign({
+//     username: '赵素葳',
+//     loveFoot: 'noodles'
+// },'ggy',{
+//     expiresIn: 60 //单位秒 这个是token的生命周期
+// })
+// console.log(token);
+
+let t = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6Iui1tee0oOiRsyIsImxvdmVGb290Ijoibm9vZGxlcyIsImlhdCI6MTczNDQxNTY5MSwiZXhwIjoxNzM0NDE1NzUxfQ.JFqbma-RJXccqQQbjeG2cjugyE-8cev-DZxEg7eEFf4'
+
+// 3 校验token
+// jwt.verify(token,加密字符,回调函数)
+jwt.verify(t, 'ggy', (err, data) => {
+    if (err) return console.log('校验失败')
+    console.log(data);
+})
+
+
+
+
+
 
 
 
